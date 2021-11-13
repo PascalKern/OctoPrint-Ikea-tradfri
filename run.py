16b2a055dbd4964f84f329054b40d523fc0615e1
Pascal Kern <pkscal.kern@gmail.com>
2021-11-13 17:07:45 +0100

    Just updates output of playground runner

--- run.py
+++ run.py
@@ -31,24 +31,34 @@ else:
         psk = TradfriClient.generate_psk(gateway=args["gateway"], identity=args["identity"],
                                          security_code=args["security_code"])
         print("Got initial PSK as: '%s'" % psk)
+        print("=" * 96)
         save_json(CONFIG, {args["gateway"]: {"identity": args["identity"], "psk": psk}})
     except (TypeError, ServerError) as e:
-        print("    Seems something is wrong with the combination of identity (%s)" % args["identity"])
         print("    Error: %s" % e)
+        print("=" * 96)
+        print("    Seems something is wrong with the combination of identity (%s)" % args["identity"])
         if 'psk' in locals():
             print("    with psk (%s) is not valid (anymore). Remove config for host: '%s' and retry!" % (psk, args["gateway"]))
         else:
             print("    with psk (%s) is not valid (anymore). Remove config for host: '%s' and retry!" % ('psk undefined', args["gateway"]))
         print("    IMPORTANT: Most likely you must change the identity too!")
+        print("=" * 96)
         exit(1)
 
 client = TradfriClient(gw_ip=args["gateway"], identity=args["identity"], psk=psk)
-print("Sockets are:")
-
+print("=" * 96)
+print("Sockets (and infos) are:")
 for socket in client.get_sockets():
+    print("-" * 10)
     print(socket)
+    print("-" * 10)
     print(socket.raw)
+    print("-" * 10)
     print(json.dumps(socket.raw, indent=2))
+    print("-" * 10)
     print(socket.device_info.raw)
+    print("-" * 10)
     print(socket.device_info.manufacturer)
+    print("-" * 10)
     print("ID: '%s | %s', Name: '%s', Type: '%s'" % (socket.path, socket.raw.get(ATTR_ID), socket.name, 'Outlet'))
+print("=" * 96)
